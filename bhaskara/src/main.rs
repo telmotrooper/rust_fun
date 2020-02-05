563d3975cc6cf807e1abb862cf2ebd934828f1b7
Telmo "Trooper <telmo.trooper@gmail.com>
2020-02-04 22:48:46 -0300

    Quit on Esc

--- bhaskara/src/main.rs
+++ bhaskara/src/main.rs
@@ -1,16 +1,20 @@
 use cursive::Cursive;
 use cursive::views::{Dialog, TextView};
+use cursive::event::Key;
 
 fn main() {
     // Creates the cursive root - required for every application
     let mut siv = Cursive::default();
 
-    siv.add_global_callback('q', |s| s.quit());
+    // Quit when "Esc" is pressed
+    siv.add_global_callback(Key::Esc, |s| s.quit());
 
     // Creates a dialog with a single "Quit" button
     siv.add_layer(Dialog::around(TextView::new("Hello, World!"))
-        .title("Cursive")
+        .title("Bhaskara Calculator")
         .button("Quit", |s| s.quit()));
+
+//    siv.add_layer(TextView::new("Press <Esc> to quit."));
 
     // Starts the event loop
     siv.run();
