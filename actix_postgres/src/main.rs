0b8f423cfd9fe3e2ad5449dafc6577ee2169072c
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-21 16:33:01 -0300

    Basic endpoints returning mocked data

--- actix_postgres/src/main.rs
+++ actix_postgres/src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
+mod employees;
+
 async fn welcome(request: HttpRequest) -> impl Responder {
     let name = request.match_info().get("name").unwrap_or("World");
     format!("Hello, {}!", &name)
@@ -9,8 +11,7 @@ async fn welcome(request: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(welcome))
-            .route("/{name}", web::get().to(welcome))
+            .configure(employees::init_routes)
     })
     .bind("127.0.0.1:8000")?
     .run()
