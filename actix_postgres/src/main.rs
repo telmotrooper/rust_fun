da09853ce29b7b9f1ae579eb0ce8025b4bcae984
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-21 15:36:44 -0300

    Added base example

--- actix_postgres/src/main.rs
+++ actix_postgres/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn welcome(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("World");
+    format!("Hello, {}!", &name)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(welcome))
+            .route("/{name}", web::get().to(welcome))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
