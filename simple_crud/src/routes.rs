b708425f99272fcd277a221ae4461bd4be461554
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-28 20:29:49 -0300

    Basic endpoint working

--- simple_crud/src/routes.rs
+++ simple_crud/src/routes.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpResponse, get, Responder};
 use actix_web::web::ServiceConfig;
+use sqlx::postgres::PgPoolOptions;
 
 use std::env;
 
@@ -13,8 +14,19 @@ async fn db_test() -> impl Responder {
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL not set.");
 
-    HttpResponse::Ok().body(database_url)
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url.as_str())
+        .await.ok()?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await.ok()?;
+
+    Some(row.0.to_string())
 }
+
 pub fn init_routes(config: &mut ServiceConfig) {
     config.service(home);
     config.service(db_test);
