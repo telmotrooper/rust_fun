107174432dbd5e53b17a32e0556391327d2dd3cb
Telmo "Trooper <telmo.trooper@gmail.com>
2021-01-14 23:28:56 -0300

    Using Box instead of Rc

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -1,10 +1,10 @@
-use std::rc::Rc;
 use std::cmp::PartialOrd;
 
 struct Node<T> {
     value: T,
-    left_child: Option<Rc<Node<T>>>,
-    right_child: Option<Rc<Node<T>>>
+    left_child: Option<Box<Node<T>>>,
+    right_child: Option<Box<Node<T>>>
+    // Box is used because Node<T> is a recursive type.
 }
 
 impl<T> Node<T> {
@@ -18,7 +18,7 @@ impl<T> Node<T> {
 }
 
 struct BinarySearchTree<T> {
-    root: Node<T>
+    root: Box<Node<T>>
 }
 
 impl<T> BinarySearchTree<T>
@@ -26,26 +26,27 @@ where T: PartialOrd
 {
     fn new(value: T) -> BinarySearchTree<T> {
         BinarySearchTree {
-            root: Node::new(value)
+            root: Box::new(Node::new(value))
         }
     }
 
     fn insert(self: &Self, value: T) {
         let new_node: Node<T> = Node::new(value);
 
-        let current_node= &self.root;
+        let mut current_node= Box::new(&self.root);
 
         if new_node.value < current_node.value {
             if let Some(left_child) = &current_node.left_child {
-                // TODO
+                todo!()
+                // current_node = left_child;
             } else {
-                // TODO
+                todo!()
             }
         } else {
             if let Some(right_child) = &current_node.right_child {
-                // TODO
+                todo!()
             } else {
-                // TODO
+                todo!()
             }
         }
     }
