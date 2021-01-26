1566057179d9c433a65e6e6f5269916167607fd4
Telmo "Trooper <telmo.trooper@gmail.com>
2021-01-26 00:09:03 -0300

    Advances regarding method 'insert'

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -36,23 +36,23 @@ where T: PartialOrd
         let mut current_node: &Option<Box<Node<T>>> = &self.root;
 
         if current_node.is_none() {
-            self.root = Some(Box::new(new_node))
+            self.root = Some(Box::new(new_node));
+            return
         }
 
-        // if new_node.value < current_node.value {
-        //     if let Some(left_child) = &current_node.left_child {
-        //         todo!()
-        //         // current_node = left_child;
-        //     } else {
-        //         todo!()
-        //     }
-        // } else {
-        //     if let Some(right_child) = &current_node.right_child {
-        //         todo!()
-        //     } else {
-        //         todo!()
-        //     }
-        // }
+        if new_node.value < current_node.as_ref().unwrap().value { // Less than
+            if let Some(left_child) = &current_node.as_ref().unwrap().left_child {
+                todo!()
+            } else {
+                todo!()
+            }
+        } else { // Greater than
+            if let Some(right_child) = &current_node.as_ref().unwrap().right_child {
+                todo!()
+            } else {
+                todo!()
+            }
+        }
     }
 }
 
@@ -60,6 +60,7 @@ fn main() {
     let mut bst: BinarySearchTree<i64> = BinarySearchTree::new();
 
     bst.insert(256);
+    bst.insert(512);
 
     println!("{:?}", bst.root.unwrap().value);
 }
