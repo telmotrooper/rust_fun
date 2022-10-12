283c54366e97d8204002903ab5c7f918620df26d
Telmo Valverde <telmo.valverde@tarmac.io>
2022-10-11 23:37:05 -0300

    Added .env and variable PORT

--- actix_again/src/main.rs
+++ actix_again/src/main.rs
@@ -1,16 +1,25 @@
 use actix_web::{web, App, HttpServer};
+use dotenvy::dotenv;
+use std::env;
 
 mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok(); // Load variables from ".env" file.
+
+    let port = env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse::<u16>()
+        .unwrap();
+
     HttpServer::new(|| {
         App::new()
             .route("/hello", web::get().to(|| async { "Hello, World!" }))
             .service(routes::hello)
             .service(routes::temperature)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
